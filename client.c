7d31a5f248d89d57512e637cee16ffc743a09dc2
sasha <ssm9960@nyu.edu>
2023-03-13 18:36:39 +0400

    same as last commit

--- client.c
+++ client.c
@@ -9,59 +9,95 @@
 
 int main()
 {
-	//socket
-	int server_sd = socket(AF_INET,SOCK_STREAM,0);
-	if(server_sd<0)
-	{
-		perror("socket:");
-		exit(-1);
-	}
-	//setsock
-	int value  = 1;
-	setsockopt(server_sd,SOL_SOCKET,SO_REUSEADDR,&value,sizeof(value)); //&(int){1},sizeof(int)
-	struct sockaddr_in server_addr;
-	bzero(&server_addr,sizeof(server_addr));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(9000);
-	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //INADDR_ANY, INADDR_LOOP
+    //socket
+    int server_sd = socket(AF_INET,SOCK_STREAM,0);
+    if(server_sd<0)
+    {
+        perror("socket:");
+        exit(-1);
+    }
+    //setsock
+    int value  = 1;
+    setsockopt(server_sd,SOL_SOCKET,SO_REUSEADDR,&value,sizeof(value)); //&(int){1},sizeof(int)
+    struct sockaddr_in server_addr;
+    bzero(&server_addr,sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(9000);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //INADDR_ANY, INADDR_LOOP
 
-	//connect
+    //connect
     if(connect(server_sd,(struct sockaddr*)&server_addr,sizeof(server_addr))<0)
     {
         perror("connect");
         exit(-1);
     }
-	
-	//accept
-	char buffer[256];
+    
+    
+    char buffer[256];
+    strcpy(buffer,"9001");
+    if(send(server_sd,buffer,strlen(buffer),0)<0)
+    {
+        perror("send");
+        exit(-1);
+    }
+    
+    close(server_sd);
+    server_addr.sin_port = htons(9000);
+    
+    server_sd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sd < 0) {
+        perror("Failed to create socket");
+        exit(EXIT_FAILURE);
+    }
 
-	while(1)
-	{
-       /*fgets(buffer,sizeof(buffer),stdin);
-       buffer[strcspn(buffer, "\n")] = 0;  //remove trailing newline char from buffer, fgets does not remove it
-       if(strcmp(buffer,"bye")==0)
-        {
-        	printf("closing the connection to server \n");
-        	close(server_sd);
-            break;
-        }
-        if(send(server_sd,buffer,strlen(buffer),0)<0)
-        {
-            perror("send");
-            exit(-1);
-        }
-        bzero(buffer,sizeof(buffer));  */
+    if(connect(server_sd,(struct sockaddr*)&server_addr,sizeof(server_addr))<0)
+    {
+        perror("Failed to connect to server");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Change the port number
+    
+    int connfd;
+    //connfd = accept(server_sd, 0, 0);
+    // Fork new processes to handle incoming connections
+    while (1) {
         
-        bzero(buffer,sizeof(buffer));
-        int bytes = recv(server_sd,buffer,sizeof(buffer),0);
-        /*if(bytes==0)   //client has closed the connection
-        {
-            printf("connection closed from server side \n");
-        }*/
-        if(bytes > 0)
-        printf("%s \n",buffer);
-        	
-	}
+        /*connfd = accept(server_sd, 0, 0);
+        if (connfd < 0) {
+            perror("Failed to accept connection");
+            exit(EXIT_FAILURE);
+        }
 
-	return 0;
+        // Fork a new process to handle the connection
+        int pid = fork();
+        if (pid < 0) {
+            perror("Failed to fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            // Child process: handle the connection
+            // Use the updated server address structure
+            if (connect(connfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+                perror("Failed to connect to server");
+                exit(EXIT_FAILURE);
+            }
+
+            // Handle the connection
+            // ...
+
+            // Close the connection
+            close(connfd);
+
+            // Exit the child process
+            exit(EXIT_SUCCESS);
+        } else {
+            // Parent process: continue accepting new connections
+            close(connfd);
+        }*/
+    }
+
+    // Close the socket
+    close(server_sd);
+
+    return 0;
 }
